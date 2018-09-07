d3c4697c73480e9344664dd477ac1bea3c088d67
Ani Mehta <ani.mehta@mail.com>
2018-09-07 13:46:29 +1000

    Base server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// Call the required packages
+const express    = require('express');
+let   app        = express();
+const bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080; // set the port
+
+var router = express.Router();
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Gandalf awaits at ' + port);
